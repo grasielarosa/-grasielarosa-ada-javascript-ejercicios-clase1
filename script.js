87bc14d956d72a3f2923a38bb92dfd9643708a74
grasielarosa <grasielarosaa@gmail.com>
2021-04-10 19:48:24 +0200

    ejercicios 1 a 3

---
+++ script.js
@@ -0,0 +1,19 @@
+// EJERCICIO 1 SALUDO - Necesitamos un programa que pida un nombre y un apellido con los siguientes mensajes respectivamente: ¿Cuál es tu nombre?.¿Cuál es tu apellido?.Con esta información, el programa deberá mostrar el siguiente mensaje: Hola {nombre} {apellido}, bienvenida a Ada
+
+//let nombre= prompt ('¿Cuál es tu nombre?'); 
+//let apellido= prompt ('¿Cuál es tu apellido?');
+//alert(`Hola ${nombre} ${apellido}, bienvenida a Ada`);
+
+//EJERCICIO 2 HELADERIA - Necesitamos un programa para una heladería que permita ingresar tres gustos de helado con los siguientes mensajes respectivamente:¿Cuál es el primer gusto?. ¿Cuál es el segundo gusto? ¿Cuál es el tercer gusto?. Con esta información, el programa deberá mostrar el siguiente mensaje: Aquí tiene su helado de {gusto1}, {gusto2} y {gusto3}
+
+//let gusto1= prompt('¿Cuál es el primer gusto?'); let gusto2= prompt('¿Cuál es el segundo gusto?');
+//let gusto3= prompt('¿Cuál es el tercer gusto?'); alert(`Aquí tiene su helado de ${gusto1}, ${gusto2} y ${gusto3}`)
+
+//EJERCICIO 3 DATOS PERSONALES Necesitamos un programa que permita ingresar nombre, apellido, edad, nacionalidad y número de documento con los siguientes mensajes respectivamente: ¿Cuál es tu nombre?. ¿Cuál es tu apellido?.¿Cuál es tu apellido?. ¿Cuál es tu edad? ¿Cuál es tu nacionalidad? ¿Cuál es tu número de documento?Con esta información, el programa deberá mostrar el siguiente mensaje: El nuevo perfil fue ingresado al sistema: nombre: {nombre}, apellido: {apellido}, edad: {edad}, nacionalidad: {nacionalidad}, número de documento: {dni}
+
+//let nombre= prompt('¿Cuál es tu nombre?');
+//let apellido= prompt ('¿Cuál es tu apellido?');
+//let edad= prompt ('¿Cuál es tu edad?');
+//let nacionalidad=prompt('¿Cuál es tu nacionalidad?');
+//let dni=prompt('¿Cuál es tu número de documento?');
+//alert (`El nuevo perfil fue ingresado al sistema: nombre: ${nombre}, apellido: ${apellido}, edad: ${edad}, nacionalidad: ${nacionalidad}, número de documento: ${dni}`)
