44494d79fa07f303aabf5975f2d405a7a702c64e
grasielarosa <grasielarosaa@gmail.com>
2021-04-10 19:55:14 +0200

    ejercicios 4 a 6

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 // EJERCICIO 1 SALUDO - Necesitamos un programa que pida un nombre y un apellido con los siguientes mensajes respectivamente: ¿Cuál es tu nombre?.¿Cuál es tu apellido?.Con esta información, el programa deberá mostrar el siguiente mensaje: Hola {nombre} {apellido}, bienvenida a Ada
-
 //let nombre= prompt ('¿Cuál es tu nombre?'); 
 //let apellido= prompt ('¿Cuál es tu apellido?');
 //alert(`Hola ${nombre} ${apellido}, bienvenida a Ada`);
 
 //EJERCICIO 2 HELADERIA - Necesitamos un programa para una heladería que permita ingresar tres gustos de helado con los siguientes mensajes respectivamente:¿Cuál es el primer gusto?. ¿Cuál es el segundo gusto? ¿Cuál es el tercer gusto?. Con esta información, el programa deberá mostrar el siguiente mensaje: Aquí tiene su helado de {gusto1}, {gusto2} y {gusto3}
-
 //let gusto1= prompt('¿Cuál es el primer gusto?'); let gusto2= prompt('¿Cuál es el segundo gusto?');
 //let gusto3= prompt('¿Cuál es el tercer gusto?'); alert(`Aquí tiene su helado de ${gusto1}, ${gusto2} y ${gusto3}`)
 
 //EJERCICIO 3 DATOS PERSONALES Necesitamos un programa que permita ingresar nombre, apellido, edad, nacionalidad y número de documento con los siguientes mensajes respectivamente: ¿Cuál es tu nombre?. ¿Cuál es tu apellido?.¿Cuál es tu apellido?. ¿Cuál es tu edad? ¿Cuál es tu nacionalidad? ¿Cuál es tu número de documento?Con esta información, el programa deberá mostrar el siguiente mensaje: El nuevo perfil fue ingresado al sistema: nombre: {nombre}, apellido: {apellido}, edad: {edad}, nacionalidad: {nacionalidad}, número de documento: {dni}
-
 //let nombre= prompt('¿Cuál es tu nombre?');
 //let apellido= prompt ('¿Cuál es tu apellido?');
 //let edad= prompt ('¿Cuál es tu edad?');
 //let nacionalidad=prompt('¿Cuál es tu nacionalidad?');
 //let dni=prompt('¿Cuál es tu número de documento?');
 //alert (`El nuevo perfil fue ingresado al sistema: nombre: ${nombre}, apellido: ${apellido}, edad: ${edad}, nacionalidad: ${nacionalidad}, número de documento: ${dni}`)
+
+//EJERCICIO 4 Nos pidieron un programa que permita ingresar el nombre de una playlist y el título de tres canciones, mediante los siguiente mensajes: ¿Cuál es el nombre de la playlist? ¿Cuál es la primer canción? ¿Cuál es la segunda canción? ¿Cuál es la tercer canción? Con está información el programa deberá mostrar el siguiente mensaje: Se ha creado la playlist {playlist} con las canciones {cancion1}, {cancion2}, {cancion3}
+//let playlist = prompt('¿Cuál es el nombre de la playlist?');
+//let cancion1 = prompt('¿Cuál es la primer canción?');
+//let cancion2=prompt('¿Cuál es la segunda canción?');
+//let cancion3=prompt('¿Cuál es la tercer canción?');
+//alert (`Se ha creado la playlist ${playlist} con las canciones ${cancion1}, ${cancion2}, ${cancion3}`);
+
+//EJERCICIO 5 Necesitamos un programa que pida ingresar los siguientes datos de una dirección: calle, número, departamento, código postal, ciudad y país, mediante los siguiente mensajes respectivamente: ¿Cuál es la calle?. ¿Cuál es el número?. ¿Cuál es el departamento? ¿Cuál es el código postal? ¿Cúal es la ciudad? ¿Cúal es el país? Con está información el programa deberá mostrar el siguiente mensaje: La dirección que ha ingresado es: {calle} {numero} {departamento}, {codigoPostal}, {ciudad}, {pais}
+//let calle=prompt('¿Cuál es la calle?');
+//let numero=prompt('¿Cuál es el número?');
+//let departamento=prompt('¿Cuál es el departamento?');
+//let codigoPostal=prompt('¿Cuál es el código postal?');
+//let ciudad=prompt('¿Cúal es la ciudad?');
+//let pais=prompt('¿Cúal es el país?');
+//alert(`La dirección que ha ingresado es: ${calle} ${numero} ${departamento}, ${codigoPostal}, ${ciudad}, ${pais}`);
+
+//EJERCICIO 6 Necesitamos un programa que pida ingresar una edad con el siguiente mensaje: Ingresá una edad por favor. El programa deberá mostrar el equivalente de esa edad en años perro mediante el siguiente mensaje El equivalente de {edad} en años perros es: {resultado}
+//let edad=prompt('Ingresá una edad por favor');
+//let resultado=edad*7;
+//alert(`El equivalente de ${edad} en años perros es: ${resultado}`)
